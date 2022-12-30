a6a940c9c033e90a84f9f3fee0e29d91b147a78f
supoa <supoa4470@gmail.com>
2022-12-30 18:42:44 +0600

    fld

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,15 @@ import { store } from "../redux/store";
 import { Provider } from "react-redux";
 import ProgressBar from "react-scroll-progress-bar"; //Add this line
 import NextNProgress from "nextjs-progressbar";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+ReactGA.initialize("347755089");
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <NextNProgress height={3} color="blue" />
