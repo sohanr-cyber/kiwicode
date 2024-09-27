818b0da6c29707a5e9bad668c46b0dd2a9d3c85f
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-09-27 19:46:44 +0600

    503285032

--- components/utils/VideoPlayer.jsx
+++ components/utils/VideoPlayer.jsx
@@ -10,7 +10,8 @@ const VideoPlayer = () => {
         style={{ width: '100%', height: 'auto', maxWidth: '500px' }}
       >
         <source
-          src='https://dw.convertfiles.com/files/0738004001727443234/quince%2025fps%20480p.mp4'
+          // src='https://dw.convertfiles.com/files/0738004001727443234/quince%2025fps%20480p.mp4'
+          src='https://firebasestorage.googleapis.com/v0/b/lms-926e5.appspot.com/o/division%2Fquince%2025fps%20480p.mp4?alt=media&token=4b6e6c55-9e61-4b06-8aca-fe4fd9a8e3be'
           type='video/mp4'
         />
       </video>
