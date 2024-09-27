d3f7d11940eafc59bd975ee3f6e80fbf94399c3d
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-09-27 11:47:52 +0600

    08508350

--- components/utils/VideoPlayer.jsx
+++ components/utils/VideoPlayer.jsx
@@ -8,7 +8,10 @@ const VideoPlayer = () => {
         width={480}
         style={{ width: '100%', height: 'auto', maxWidth: '500px' }}
       >
-        <source src='/assets/quince-promo.mp4' type='video/mp4' />
+        <source
+          src='https://firebasestorage.googleapis.com/v0/b/lms-926e5.appspot.com/o/division%2Fquince-promo.mp4?alt=media&token=b3afef54-1d48-461a-8c7c-43792066be6b'
+          type='video/mp4'
+        />
       </video>
     </div>
   )
