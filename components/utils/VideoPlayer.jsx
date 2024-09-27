9ec6e5fcec65a342975eeb276a90e451606a609c
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-09-27 19:24:37 +0600

    083205304

--- components/utils/VideoPlayer.jsx
+++ components/utils/VideoPlayer.jsx
@@ -9,7 +9,10 @@ const VideoPlayer = () => {
         width={480}
         style={{ width: '100%', height: 'auto', maxWidth: '500px' }}
       >
-        <source src='ad/promo.mp4' type='video/mp4' />
+        <source
+          src='https://dw.convertfiles.com/files/0738004001727443234/quince%2025fps%20480p.mp4'
+          type='video/mp4'
+        />
       </video>
     </div>
   )
